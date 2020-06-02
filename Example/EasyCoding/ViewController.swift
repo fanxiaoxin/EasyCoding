95a060a6cf097c99720fc1b2c844589ec797e197
fanxx <fanxiaoxin_1987@126.com>
2020-06-02 23:41:35 +0800

    @_functionBuilder测试代码

--- Example/EasyCoding/ViewController.swift
+++ Example/EasyCoding/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        TEST().haha()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,13 +25,44 @@ class ViewController: UIViewController {
 }
 
 @_functionBuilder
-class A  {
-    var buildBlock: () -> Void
+struct AttributedStringBuilder {
+    static func buildBlock() -> NSAttributedString {
+        return NSAttributedString(string: "I'm Default")
+    }
+  static func buildBlock(_ segments: NSAttributedString...) -> NSAttributedString {
+    let string = NSMutableAttributedString()
+    segments.forEach { string.append($0) }
+    return string
+  }
+static func buildBlock(_ string: String,_ color: UIColor) -> NSAttributedString {
+    return NSAttributedString(string: string, attributes: [NSAttributedString.Key.foregroundColor : color])
+  }
+}
+extension NSAttributedString {
+  convenience init(@AttributedStringBuilder _ content: () -> NSAttributedString) {
+    self.init(attributedString: content())
+  }
 }
 
 class TEST {
-    @A
     func haha() {
-        
+        let hello = NSAttributedString(string: "Hello")
+        let world = NSAttributedString(string: "World")
+       let str =
+        NSAttributedString {
+          hello
+          world
+        }
+        print(str)
+        let str2 = NSAttributedString{
+            "fuck you"
+            UIColor.red
+        }
+        print(str2)
+        let str3 = NSAttributedString { () -> NSAttributedString in
+            "SHit"
+            UIColor.blue
+        }
+        print(str3)
     }
 }
