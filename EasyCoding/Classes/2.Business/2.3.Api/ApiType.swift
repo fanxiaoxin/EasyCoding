5a81ab137861517a00f03a90072b6dc9b0bdb767
Fanxx <fanxiaoxin_1987@126.com>
2020-06-18 15:31:39 +0800

    Coding...

--- EasyCoding/Classes/2.Business/2.3.Api/ApiType.swift
+++ EasyCoding/Classes/2.Business/2.3.Api/ApiType.swift
@@ -65,27 +65,8 @@ public protocol ECResponseApiType: ECSimpleApiType {
     associatedtype ResponseType: ECApiResponseType
 }
 public protocol ECPagedResponseApiType: ECResponseApiType, ECApiPagedListRequestType where Self.ResponseType: ECApiPagedListResponseType {
-    ///页面请求结构
-//    associatedtype PagedInfoType: ECApiPagedListRequestType
-//    var page:PagedInfoType { get set }
+    
 }
-//extension ECPagedResponseApiType {
-//    public var parameters: [String : Any]? {
-//        let json = self.toJSON()
-//        let page = self.page.toJSON()
-//        guard var json1 = json else {
-//            return page
-//        }
-//        guard let json2 = page else {
-//            return json
-//        }
-//        json1.merge(json2, uniquingKeysWith: { v1,_ in v1 })
-//        return json1
-//    }
-//    public mutating func mapping(mapper: HelpingMapper) {
-//        mapper >>> self.page
-//    }
-//}
 ///上传接口
 public protocol ECUploadApiType: ECSimpleApiType {
     var datas: [MultipartFormData] { get }
