bf9d6eaf0af4a1eea03e57dd2c1ac215d7cad97e
Fanxx <fanxiaoxin_1987@126.com>
2020-07-21 09:55:16 +0800

    调用ECViewControllerAccessable的实现方法

--- EasyCoding/Classes/2.Business/2.2.AccessControl/2.2.1.AccessControl/2.2.1.1 Preconditions/ECSimplePrecondition.swift
+++ EasyCoding/Classes/2.Business/2.2.AccessControl/2.2.1.AccessControl/2.2.1.1 Preconditions/ECSimplePrecondition.swift
@@ -8,17 +8,17 @@
 import UIKit
 
 public class ECSimplePrecondition<IT,OT>: ECPrecondition<IT,OT> {
-    var verfiyAction: ((ECSimplePrecondition<IT,OT>, @escaping (Bool)->Void)->Void)? = nil
-    public init(_ action: @escaping (ECSimplePrecondition<IT,OT>, @escaping (Bool)->Void)->Void) {
+    var verfiyAction: ((ECSimplePrecondition<IT,OT>)->Void)? = nil
+    public init(_ action: @escaping (ECSimplePrecondition<IT,OT>)->Void) {
         super.init()
         self.verfiyAction = action
     }
     public override func verify() {
-        self.verfiyAction?(self, self.finished)
+        self.verfiyAction?(self)
     }
 }
 extension ECPrecondition {
-    public static func easy(action: @escaping (ECSimplePrecondition<InputType,OutputType>, @escaping (Bool)->Void)->Void) -> ECSimplePrecondition<InputType, OutputType> {
+    public static func easy(action: @escaping (ECSimplePrecondition<InputType,OutputType>)->Void) -> ECSimplePrecondition<InputType, OutputType> {
         return ECSimplePrecondition<InputType,OutputType>(action)
     }
 }
--- EasyCoding/Classes/2.Business/2.2.AccessControl/2.2.4.ViewControllerAccess/ECViewControllerAccessable.swift
+++ EasyCoding/Classes/2.Business/2.2.AccessControl/2.2.4.ViewControllerAccess/ECViewControllerAccessable.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 public protocol ECViewControllerAccessable {
+    ///加载视图的源视图
+    var accessSourceViewController: UIViewController { get }
+    ///加载目标视图
     func load(_ destination: ECViewControllerType, segue: ECPresentSegue?, otherwise: (()->Void)?)
 }
+extension ECViewControllerAccessable {
+    ///加载视图的源视图
+    public var accessSourceViewController: UIViewController { return UIViewController.easy.current! }
 
-extension UIViewController: ECViewControllerAccessable {
+    public func load(_ destination: ECViewControllerType, segue: ECPresentSegue? = nil) {
+        self.load(destination, segue: segue, otherwise: nil)
+    }
+    public func load(_ destination: ECViewControllerType, otherwise: (()->Void)?) {
+        self.load(destination, segue: nil, otherwise: otherwise)
+    }
+}
+///实现load方法
+extension ECViewControllerAccessable {
     private func check(preconditions destination: ECViewControllerType, pass: @escaping ()->Void) {
         self.check(preconditions: destination, pass: pass, otherwise: nil)
     }
@@ -28,8 +42,9 @@ extension UIViewController: ECViewControllerAccessable {
             var newCs = conditions
             let cdt = newCs.removeFirst()
             cdt.failureHandler = otherwise
-            ECViewControllerAccessCenter.shared.check(condition: cdt, input: self) { [weak self] (_) in
-                self?.check(conditions: newCs, pass: pass, otherwise: otherwise)
+            let source = self.accessSourceViewController
+            ECViewControllerAccessCenter.shared.check(condition: cdt, input: source) { [weak source] (_) in
+                source?.check(conditions: newCs, pass: pass, otherwise: otherwise)
             }
         }else{
             pass()
@@ -37,15 +52,22 @@ extension UIViewController: ECViewControllerAccessable {
     }
     ///不判断前提要求，直接加载
     public func directLoad(_ destination: ECViewControllerType, segue: ECPresentSegue? = nil) {
-        self.easy.present(destination, segue: segue ?? destination.segue)
+        self.accessSourceViewController.easy.present(destination, segue: segue ?? destination.segue)
     }
     ///判断权限通过后加载
     public func load(_ destination: ECViewControllerType, segue: ECPresentSegue?, otherwise: (()->Void)?) {
-        self.check(preconditions: destination, pass: { [weak self] in
-            self?.directLoad(destination, segue: segue)
+        let source = self.accessSourceViewController
+        self.check(preconditions: destination, pass: { [weak source] in
+            source?.directLoad(destination, segue: segue)
         }, otherwise: otherwise)
     }
 }
+///UIViewController使用自身
+extension UIViewController: ECViewControllerAccessable {
+    ///加载视图的源视图
+    public var accessSourceViewController: UIViewController { return self }
+}
+
 extension ECViewControllerType {
     ///关闭页面
     public func unload() {
@@ -53,21 +75,14 @@ extension ECViewControllerType {
     }
 }
 
-extension ECViewControllerAccessable {
-    public func load(_ destination: ECViewControllerType, segue: ECPresentSegue?, otherwise: (()->Void)?) {
-        UIViewController.easy.current?.load(destination, segue: segue, otherwise: otherwise)
-    }
-}
-extension ECViewControllerAccessable {
-    public func load(_ destination: ECViewControllerType, segue: ECPresentSegue? = nil) {
-        self.load(destination, segue: segue, otherwise: nil)
-    }
-    public func load(_ destination: ECViewControllerType, otherwise: (()->Void)?) {
-        self.load(destination, segue: nil, otherwise: otherwise)
-    }
-}
 extension ECPrecondition: ECViewControllerAccessable where InputType: UIViewController {
-    public func load(_ destination: ECViewControllerType, segue: ECPresentSegue?, otherwise: (() -> Void)?) {
-        (self.input ?? UIViewController.easy.current)?.load(destination, segue: segue, otherwise: otherwise)
+    public var accessSourceViewController: UIViewController { return self.currentController! }
+}
+
+extension ECPrecondition where InputType: UIViewController {
+    ///重定向到新视图，自身不显示
+    public func redirect(to controller: ECViewControllerType, segue: ECPresentSegue? = nil, otherwise: (() -> Void)? = nil) {
+        self.load(controller, segue: segue, otherwise: otherwise)
+        self.finished(false)
     }
 }
